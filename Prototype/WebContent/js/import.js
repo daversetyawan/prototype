7c2f35ef748578d419d5a7bd6037ffa2c6567a82
devri.rs <devri.rs@010ACE1013.nu-ace.ad-ins.com>
2014-03-04 09:13:24 +0700

    simple import javascript and css, for better security, i think

---
+++ Prototype/WebContent/js/import.js
@@ -0,0 +1,55 @@
+function importFile(filename) {
+	var filetype = filename.substring(filename.lastIndexOf('.') + 1);
+	var tag = '';
+
+	switch (filetype) {
+	case 'css':
+		tag = document.createElement('link');
+		tag.setAttribute('rel', 'stylesheet');
+		tag.setAttribute('type', 'text/css');
+		tag.setAttribute('href', filename);
+		break;
+
+	case 'js':
+		tag = document.createElement('script');
+		tag.setAttribute('type', 'text/javascript');
+		tag.setAttribute('src', filename);
+		break;
+
+	default:
+		break;
+	}
+
+	if (tag !== undefined) {
+		var head = document.getElementsByTagName('head')[0];
+		head.appendChild(tag);
+	}
+}
+
+var base = location.pathname.match(/\/\w+/i);
+
+//Import CSS Files
+importFile(base + '/css/metro-bootstrap.css');
+importFile(base + '/css/metro-bootstrap-responsive.css');
+importFile(base + '/css/style.css');
+importFile(base + '/css/selectize.css');
+
+//Import JS Files
+importFile(base + '/js/jquery/jquery.min.js');
+setTimeout(function() {
+	importFile(base + '/js/jquery/jquery.widget.min.js');
+	importFile(base + '/js/metro/metro.min.js');
+	importFile(base + '/js/rumble/jquery.jrumble.1.3.min.js');
+	importFile(base + '/js/metro/metro-calendar.min.js');
+	importFile(base + '/js/metro/metro-datepicker.min.js');
+	importFile(base + '/js/selectize/selectize.js');
+	importFile(base + '/js/selectize/index.js');
+	importFile(base + '/js/other/selectize.js');
+	importFile(base + '/js/other/popup.js');
+	importFile(base + '/js/other/script.js');
+}, 500);
+
+
+
+
+
---
+++ Prototype/WebContent/js/other/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("#datepicker-begin, #datepicker-end").datepicker({
+		date : "2014-01-01",
+		format : "dd/mm/yyyy",
+		effect : "none",
+		position : "bottom"
+	});
+});
